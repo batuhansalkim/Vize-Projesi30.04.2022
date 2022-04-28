32215d2b2ab2697932949ef9882fe7e461239f20
Baturpcu <batusalkim11@gmail.com>
2022-04-28 20:22:03 +0300

    web-api
    web-api

--- vizeprojesi/ViewModel/KayitModel.cs
+++ vizeprojesi/ViewModel/KayitModel.cs
@@ -10,5 +10,7 @@ namespace vizeprojesi.ViewModel
         public string kayitId { get; set; }
         public string kayitDersiAlanOgrId { get; set; }
         public string kayitDerslerId { get; set; }
+        public DersiAlanOgrModel ogrBilgi { get; set; }
+        public DerslerModel dersBilgi { get; set; }
     }
 }
